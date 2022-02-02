03e15e03dc545e56aa22da53e66115d1c82ca445
ezra citron <ezracitron@gmail.com>
2022-02-02 21:39:28 +0000

    cleaning code

--- easy_wordle/wordle.py
+++ easy_wordle/wordle.py
@@ -1,5 +1,9 @@
 from typing import Dict
 import random
+import json
+
+with open('data/word_freq.json') as f:
+    word_freq = json.loads(f.read())
 
 class Wordle:
     # dl = disallowed_letters, rp=right_place, rp=wrong_place
@@ -19,30 +23,10 @@ class Wordle:
             self.wp: Dict[int, str] = dict()
         if input is None:
             self.input = list()
-
-        # if input is None:
-        #     self.input = input
-
         if possible_words is None:
             self.possible_words = self._get_word_list()
 
         self.possible_words_len = len(self.possible_words)
-
-        # self.test_input = {
-        #     0: {"letter": "a", "color": "b"},
-        #     1: {"letter": "u", "color": "b"},
-        #     2: {"letter": "d", "color": "b"},
-        #     3: {"letter": "i", "color": "b"},
-        #     4: {"letter": "g", "color": "b"},
-        # }
-
-        # self.test_input2 = {
-        #     0: {"letter": "f", "color": "b"},
-        #     1: {"letter": "a", "color": "b"},
-        #     2: {"letter": "d", "color": "b"},
-        #     3: {"letter": "i", "color": "b"},
-        #     4: {"letter": "y", "color": "g"},
-        # }
 
     # def get_good_word_picks(self):
     #     self.good_word_picks = [
@@ -61,15 +45,20 @@ class Wordle:
     @staticmethod
     def any_letters_in_word(letters: str, word: str) -> bool:
         """
-        check to see if any letter in a string are in a word
+        check to see if any letter in a string are in a word.
         any_letters_in_word('gkt','kerb') return True, since k is in kerb
         """
         letters_bool = [i in word for i in letters]
         return any(letters_bool)
 
     def get_user_input(self):
+        """
+        Asks for a users word and colors and the formats into a dictionary.
+        Places into the input list
+        """
         input_dict = dict()
         word = input("Type your last word guess: ")
+
         if len(word)!=5:
             raise Exception("Word must be 5 letters")
         colors = input("Type the colors of the letters: ")
@@ -80,21 +69,39 @@ class Wordle:
             input_dict[x] = dict( [ ('letter',i[0]),('color',i[1] ) ] )
         
         self.input.append(input_dict)
+
         return None
 
-    def print_possible_words_examples(self,n=5)->None:
-        print(random.sample(self.possible_words,n))
+    def print_words_examples(self,n=5)->None:
+        print("\n### EXAMPLE WORDS ###")
+        try:
+            print(random.sample(self.possible_words,min(len(self.possible_words),n)))
+        except ValueError as e:
+            print('There are less that 5 examples, showing 1 example')
+
+
+    def print_best_picks(self,n=5)->None:
+      
+        print("\n### BEST PICK WORDS ###")
+        no_dupe_letters= [word for word in self.possible_words if len(''.join(set(word)))==5  ]
+        word_with_freq = [( w, self.freq_score(w) ) for w in no_dupe_letters]
+        word_with_freq.sort(key=lambda x:-x[1])
+        # max_freq= max(word_with_freq,key = lambda x:x[1])[1] 
+        best_picks = [ w[0] for w in word_with_freq[0:min(5,len(word_with_freq))]  ]
+        print(", ".join(best_picks))
+
 
     def print_current_state(self)->None:
-        print(f"""
-### CURRENT STATE ###
-
-{len(self.possible_words)} / {self.possible_words_len} words available ({ round(100* (len(self.possible_words) / self.possible_words_len),3) }%).\n
-Possible example words: {w.print_possible_words_examples()}
-### CURRENT STATE ###
-             """     
+        word_perc = round(100* (len(self.possible_words) / self.possible_words_len),3)
+        print(f"""\n### CURRENT STATE ###\n{len(self.possible_words)} / {self.possible_words_len} words available ({word_perc }%).\n### CURRENT STATE ###"""     
         )
         return None
+    
+    @staticmethod
+    def freq_score(word):
+        return sum([word_freq[letter] for letter in word])
+
+
 
     def dl_rule(self):
         """Using the disallowed letters input to change the state of possible words"""
@@ -127,17 +134,13 @@ Possible example words: {w.print_possible_words_examples()}
         return None
 
     def check_letter_in_other_categories(self,cats_to_check:list)-> bool:
-        print(cats_to_check)
         return False
 
     def parse_input(self, input):
         for i in input:
-            if input[i]["color"] == "b":
-                if self.check_letter_in_other_categories(cats_to_check=['g','y']):
-                    raise Exception
 
-                elif not (input[i]["letter"] in self.dl):
-                    self.dl.append(input[i]["letter"])    
+            if input[i]["color"] == "b":
+                self.dl.append(input[i]["letter"])    
 
             if input[i]["color"] == "g":
                 self.rp[i] = input[i]["letter"]
@@ -145,15 +148,20 @@ Possible example words: {w.print_possible_words_examples()}
             if input[i]["color"] == "y":
                 self.wp[i] = input[i]["letter"]
 
-
-if __name__ == "__main__":
+def main():
     w = Wordle()
-
     while True:
         w.get_user_input()
-        w.parse_input(w.input[-1])
-        w.update_possible_words()
+        w.parse_input(w.input[-1]) #parse the most recent input
+        w.update_possible_words() #update the state
         w.print_current_state()
+        w.print_words_examples(n= 5)
+        w.print_best_picks(n= 3)
+
+
+
+if __name__ == "__main__":
+    main()
 
 
 
