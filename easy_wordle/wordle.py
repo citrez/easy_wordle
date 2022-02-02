9482ec9f12b9129ee42afb835397010de024d18a
ezra citron <ezracitron@gmail.com>
2022-02-02 18:07:20 +0000

    working game

--- easy_wordle/wordle.py
+++ easy_wordle/wordle.py
@@ -8,7 +8,7 @@ class Wordle:
         rp=None,
         wp=None,
         possible_words=None,
-        input_dict=None,
+        input: list[Dict]=None,
     ):
         if dl is None:
             self.dl = list()
@@ -16,70 +16,79 @@ class Wordle:
             self.rp: Dict[int, str] = dict()
         if wp is None:
             self.wp: Dict[int, str] = dict()
-        if input_dict is None:
-            self.input_dict = dict()
+        if input is None:
+            self.input = list()
 
-        # if input_dict is None:
-        #     self.input_dict = input_dict
+        # if input is None:
+        #     self.input = input
 
         if possible_words is None:
             self.possible_words = self.get_word_list()
 
-        self.test_input = {
-            0: {"letter": "a", "color": "b"},
-            1: {"letter": "u", "color": "b"},
-            2: {"letter": "d", "color": "b"},
-            3: {"letter": "i", "color": "b"},
-            4: {"letter": "g", "color": "b"},
-        }
+        self.possible_words_len = len(self.possible_words)
 
-        self.test_input2 = {
-            0: {"letter": "f", "color": "b"},
-            1: {"letter": "a", "color": "b"},
-            2: {"letter": "d", "color": "b"},
-            3: {"letter": "i", "color": "b"},
-            4: {"letter": "y", "color": "g"},
-        }
+        # self.test_input = {
+        #     0: {"letter": "a", "color": "b"},
+        #     1: {"letter": "u", "color": "b"},
+        #     2: {"letter": "d", "color": "b"},
+        #     3: {"letter": "i", "color": "b"},
+        #     4: {"letter": "g", "color": "b"},
+        # }
 
-    def get_good_word_picks(self):
-        self.good_word_picks = [
-            word for word in self.possible_words if len(set(word)) == 5
-        ]
+        # self.test_input2 = {
+        #     0: {"letter": "f", "color": "b"},
+        #     1: {"letter": "a", "color": "b"},
+        #     2: {"letter": "d", "color": "b"},
+        #     3: {"letter": "i", "color": "b"},
+        #     4: {"letter": "y", "color": "g"},
+        # }
+
+    # def get_good_word_picks(self):
+    #     self.good_word_picks = [
+    #         word for word in self.possible_words if len(set(word)) == 5
+    #     ]
 
     def get_word_list(self):
         with open("/usr/share/dict/words") as f:
             word_list = [line.rstrip("\n") for line in f]
 
-        word_list = list(set([word.lower() for word in word_list]))
-
-        word_list = [word for word in word_list if len(word) == 5]
-        word_list.sort()
-
-        self.word_list_length = len(word_list)
-
+        word_list = list(set([word.lower() for word in word_list])) # turn all words lowercase and remove dupes
+        word_list = [word for word in word_list if len(word) == 5] # only look at 5 letter words
+        word_list.sort() # sort alphabetically
         return word_list
 
     @staticmethod
     def any_letters_in_word(letters: str, word: str) -> bool:
+        """
+        check to see if any letter in a string are in a word
+        any_letters_in_word('gkt','kerb') return True, since k is in kerb
+        """
         letters_bool = [i in word for i in letters]
         return any(letters_bool)
 
     def get_user_input(self):
+        input_dict = dict()
         word = input("Type your last word guess: ")
+        if len(word)!=5:
+            raise Exception("Word must be 5 letters")
         colors = input("Type the colors of the letters: ")
+        if len(colors)!=5:
+            raise Exception("There must be exactly 5 colors")
 
         for x,i in enumerate(tuple(zip(word,colors) ) ):
-            self.input_dict[x] = dict( [ ('letter',i[0]),('color',i[1] ) ] )
+            input_dict[x] = dict( [ ('letter',i[0]),('color',i[1] ) ] )
+        
+        self.input.append(input_dict)
         return None
 
     def print_possible_words_examples()->None:
         pass
 
-    def print_current_state(self):
+    def print_current_state(self)->None:
         print(f"""
 ### CURRENT STATE ###
 
-{len(self.possible_words)} / {self.word_list_length} words available ({ round(100* (len(self.possible_words) / self.word_list_length),3) }%).\n
+{len(self.possible_words)} / {self.possible_words_len} words available ({ round(100* (len(self.possible_words) / self.possible_words_len),3) }%).\n
 The disallowed letters are {self.dl}.
 The correct letters are: {self.rp}
 The correct letters, wrong place are: {self.wp}
@@ -87,8 +96,6 @@ The correct letters, wrong place are: {self.wp}
 ### CURRENT STATE ###
              """     
         )
-
-
         return None
 
     def dl_rule(self):
@@ -100,30 +107,32 @@ The correct letters, wrong place are: {self.wp}
         # these funcs update state
         updated_possible_words = list()
         for i in self.rp:
-            # self.possible_words = [word for word in self.possible_words if word[i] == self.rp[i]]
-            for word in self.possible_words:
-                if (word[i] == self.rp[i]):
-                    print(word)
-                    updated_possible_words.append(word)
-            self.possible_words = updated_possible_words
+            self.possible_words = [word for word in self.possible_words if word[i] == self.rp[i]]
+            # for word in self.possible_words:
+            #     if (word[i] == self.rp[i]):
+            #         updated_possible_words.append(word)
+            # self.possible_words = updated_possible_words
 
         return None
 
+    def wp_check(self, word, n):
+        """Check if the word has the letter in the position of the wrong place letter"""
+        # print(self.wp)
+        check = (word[n] != self.wp[n]) and (self.wp[n] in word)
+        return check
+
     def wp_rule(self):
-
-        def wp_check(self, word, n):
-            check = (word[n] != self.wp[n]["letter"]) and (self.wp[n]["letter"] in word)
-            return check
-
+        print(self.wp)
         for i in self.wp:
-            self.possible_words = [word for word in self.possible_words if wp_check(word,i) ]
-            self.possible_words = [word for word in self.possible_words if self.test_input[i]["letter"] in word]
+            self.possible_words = [word for word in self.possible_words if self.wp_check(word,i) ] #check that the word does have the letter in the position we know its not in
+            self.possible_words = [word for word in self.possible_words if self.wp[i] in word] # check that the letter is indeed in the word
         return None
 
 
     def update_possible_words(self)->None:
         self.dl_rule()
         self.rp_rule()
+        self.wp_rule()
         return None
 
    
@@ -131,23 +140,23 @@ The correct letters, wrong place are: {self.wp}
         print(cats_to_check)
         return False
 
-    def parse_input_dict(self, input_dict):
+    def parse_input(self, input):
         print("Parsing Input:")
 
-        for i in input_dict:
+        for i in input:
 
-            if input_dict[i]["color"] == "b":
+            if input[i]["color"] == "b":
                 if self.check_letter_in_other_categories(cats_to_check=['g','y']):
                     raise Exception
 
-                elif not (input_dict[i]["letter"] in self.dl):
-                    self.dl.append(input_dict[i]["letter"])    
+                elif not (input[i]["letter"] in self.dl):
+                    self.dl.append(input[i]["letter"])    
 
-            if input_dict[i]["color"] == "g":
-                self.rp[i] = input_dict[i]["letter"]
+            if input[i]["color"] == "g":
+                self.rp[i] = input[i]["letter"]
 
-            if input_dict[i]["color"] == "y":
-                self.wp[i] = input_dict[i]["letter"]
+            if input[i]["color"] == "y":
+                self.wp[i] = input[i]["letter"]
 
 
 if __name__ == "__main__":
@@ -155,15 +164,13 @@ if __name__ == "__main__":
 
     # print(w.possible_words[0:5])
     # w.print_current_state()
-    w.get_user_input()
-    print(w.input_dict)
+    while True:
+        w.get_user_input()
+        w.parse_input(w.input[-1])
+        w.update_possible_words()
+        w.print_current_state()
 
-    # w.parse_input_dict(w.test_input)
-    # w.update_possible_words()
-
-    # w.print_current_state()
-
-    # w.parse_input_dict(w.test_input2)
+    # w.parse_input(w.test_input2)
     # w.update_possible_words()
 
     # w.print_current_state()
